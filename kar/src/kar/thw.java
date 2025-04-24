6275011810954ea88b11515b3cdd9b12180bae17
DELL <DELL@DESKTOP-SDTK6GM>
2025-04-24 11:46:43 +0530

    push Commit.

--- kar/src/kar/thw.java
+++ kar/src/kar/thw.java
@@ -2,6 +2,7 @@ package kar;
 
 public class thw {
     public static void main(String[] args) {
-        System.out.println("hello world"); // Corrected line
+        System.out.println("hello world");
+        System.out.println("New world");// Corrected line
     }
 }
